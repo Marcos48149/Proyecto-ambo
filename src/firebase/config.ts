d9d6f4494911321f0225b7ee825a7f0ea109ef2a
Marcos Gómez <marcosgomez48149@gmail.com>
2025-10-03 12:57:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9653193764-eafac",
+  "appId": "1:653248034839:web:eaa6a1ec754d1ff5e78452",
+  "apiKey": "AIzaSyDApRRl6PUrqXPG4xuk2la5CIPH1atnnGQ",
+  "authDomain": "studio-9653193764-eafac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "653248034839"
+};
